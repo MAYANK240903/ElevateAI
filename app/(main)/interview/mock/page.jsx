d7aa296f5167933f7c4f84f947fc87add5d3e81e
MAYANK240903 <mayanksaksham246@gmail.com>
2025-02-03 00:27:35 +0530

    all route pages and not found page

---
+++ app/(main)/interview/mock/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MockInterviePage = () => {
+  return (
+    <div>
+      MockInterviewPage
+    </div>
+  )
+}
+
+export default MockInterviePage
---
+++ app/(main)/interview/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InterviePage = () => {
+  return (
+    <div>
+      InterviePage
+    </div>
+  )
+}
+
+export default InterviePage
