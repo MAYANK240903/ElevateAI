e39adfc80a049cf7b2855a12bcdf18544b7665c7
MAYANK240903 <mayanksaksham246@gmail.com>
2025-02-07 20:41:41 +0530

    resume section complete

--- app/(main)/resume/_components/resume-builder.jsx
+++ app/(main)/resume/_components/resume-builder.jsx
@@ -1,20 +1,28 @@
 "use client";
 
-import { saveResume } from '@/actions/resume';
-import { resumeSchema } from '@/app/lib/schema';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { Textarea } from '@/components/ui/textarea';
-import useFetch from '@/hooks/use-fetch';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { AlertTriangle, Download, Edit, Loader2, Monitor, Save } from 'lucide-react';
-import React, { useEffect, useState } from 'react'
-import { Controller, useForm } from 'react-hook-form';
-import EntryForm from './entry-form';
-import { entriesToMarkdown } from '@/app/lib/helper';
-import MDEditor from '@uiw/react-md-editor';
-import { useUser } from '@clerk/nextjs';
+import { useState, useEffect } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  AlertTriangle,
+  Download,
+  Edit,
+  Loader2,
+  Monitor,
+  Save,
+} from "lucide-react";
+import { toast } from "sonner";
+import MDEditor from "@uiw/react-md-editor";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+import { saveResume } from "@/actions/resume";
+import EntryForm from "./entry-form";
+import useFetch from "@/hooks/use-fetch";
+import { useUser } from "@clerk/nextjs";
+import { entriesToMarkdown } from "@/app/lib/helper";
+import { resumeSchema } from "@/app/lib/schema";
 import html2pdf from "html2pdf.js/dist/html2pdf.min.js";
 
 export default function ResumeBuilder({ initialContent }) {
